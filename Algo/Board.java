d3c93e89596a8e46d57bca9651efc0236aed92fe
Mahas1 <mahasvan@gmail.com>
2023-10-06 20:17:49 +0530

    add base files

---
+++ Algo/Board.java
@@ -0,0 +1,15 @@
+package Algo;
+
+public class Board {
+    private int size;
+    private int[][] board;
+
+    public Board() {
+        System.out.println("Constructor");
+    }
+
+    void printBoard() {
+        System.out.println("This should print the board... soon");
+    }
+
+}
