ca6dfb164faa9f9d8ea0cddb7a342ba5620cce4e
Mahas1 <mahasvan@gmail.com>
2023-10-06 20:43:07 +0530

    deal with warnings

--- Algo/Grid.java
+++ Algo/Grid.java
@@ -3,7 +3,7 @@ package Algo;
 import java.util.ArrayList;
 
 public class Grid {
-    private int size;
+    private final int size;
     private ArrayList<ArrayList<String>> grid;
     private static final String space = "　";
     private static final String pipe = "｜";
@@ -13,13 +13,13 @@ public class Grid {
     public Grid(int size) {
         this.size = size;
         this.grid = new ArrayList<>();
-         this.horiz = new String(new char[size*2-1]).replace("\0", this.hyphen);
+         this.horiz = new String(new char[size*2-1]).replace("\0", hyphen);
 
         // populate the board with emptiness
         for (int i=0; i<size; i++) {
             ArrayList<String> temp = new ArrayList<>();
             for (int j = 0; j < size; j++) {
-                temp.add(this.space);
+                temp.add(space);
             }
             this.grid.add(temp);
         }
@@ -28,10 +28,10 @@ public class Grid {
 
     public void printBoard() {
         for (int i=0; i<size-1; i++) {
-            System.out.println(String.join(this.pipe, grid.get(i)));
+            System.out.println(String.join(pipe, grid.get(i)));
             System.out.println(this.horiz);
         }
-        System.out.println(String.join(this.pipe, grid.get(this.size-1)));
+        System.out.println(String.join(pipe, grid.get(this.size-1)));
     }
 
 }
