7ba6851123d664b4e88fd9db8b9bf3666e683bdb
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-10-07 12:00:51 +0530

    added checkEmpty call to Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+package Algo;
+
 import Algo.Grid;
 
 public class Main {
     public static void main(String[] args) {
         Grid board = new Grid(3);
         board.printBoard();
+        //i've added checkEmpty as a direct call here, later needs to be put inside the function for making a move?
+        board.checkEmpty();
     }
 }
