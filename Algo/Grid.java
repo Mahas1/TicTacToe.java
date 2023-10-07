dc833532875214fb117e22197bc99da1926dd67c
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-10-07 14:06:35 +0530

    Update Grid.java

--- Algo/Grid.java
+++ Algo/Grid.java
@@ -36,16 +36,64 @@ public class Grid {
     }
     
     
-    public boolean checkEmpty() {
-    	for (ArrayList<String> list : this.grid) {
-            for (String element : list) {
-                if (element==space) {
-                    // If element is a space, return true
-                    return true;
-                }
-            }
-        }
-        return false;
+    		
+
+    
+    public ArrayList<ArrayList<Integer>> checkEmpty() {
+    	int i,j;
+    	ArrayList<ArrayList<Integer>> lst = new ArrayList<ArrayList<Integer>>();
+    	
+    	for(i=0;i<this.size;i++) {
+    		for(j=0;j<this.size;j++) {
+    			if (this.grid.get(i).get(j)==space){
+    				ArrayList<Integer> sublst = new ArrayList<Integer>();
+    				sublst.add(i);
+    				sublst.add(j);
+    				lst.add(sublst);		
+    			}
+    		}
+    	}
+    	return lst;
+    }
+
+
+    //errors still exist in this fxn, will update soon
+    public boolean checkForWin() {
+    	boolean win=false;
+    	int flag=0,i,j;
+    			
+    	//row wise victory check
+    	for(i=0;i<this.size;i++) {
+    		for (j=0;j<this.size-1;j++) {
+    			if ((this.grid[i][j]!=" ") and (this.grid[i][j]==this.grid[i][j+1]){
+    				flag=1;
+    			}
+    			else {
+    				flag=0;
+    				break;
+    			}
+    		} 		
+    	}
+    	//column wise victory check
+    	for(j=0;j<this.size;j++) {
+    		for (i=0;i<this.size-1;i++) {
+    			if ((this.grid[i][j]!=" ") and (this.grid[i][j]==this.grid[i+1][j]){
+    				flag=1;
+    			}
+    			else {
+    				flag=0;
+    				break;
+    			}
+    		} 		
+    	}
+    	
+    	if (flag==1) {
+    		win=true;
+    	}
+    	
+    	return win;
+    	
+    	
     }
 }
 
