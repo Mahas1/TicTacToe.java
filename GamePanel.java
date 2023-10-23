5c7240d3cd5cd299d347b8c4e88715197d424d12
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-10-23 20:27:53 +0530

    Create GamePanel.java

---
+++ GamePanel.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import Grid.java;
+
+public class GamePanel extends JPanel {
+    private final Grid board;
+    private JButton[][] buttons;
+
+    public GamePanel(Grid board) {
+        this.board = board;
+        int size = board.getSize();
+        buttons = new JButton[size][size];
+
+        setLayout(new GridLayout(size, size));
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                JButton button = new JButton();
+                buttons[i][j] = button;
+                button.setFont(new Font("Arial", Font.PLAIN, 40));
+                button.addActionListener(new ButtonClickListener(i, j));
+                add(button);
+            }
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int size = board.getSize();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                buttons[i][j].setText(board.getGridValue(i, j));
+            }
+        }
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        private final int x;
+        private final int y;
+
+        public ButtonClickListener(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (board.canPlace(x, y)) {
+                board.makeMove(x, y);
+                repaint();
+
+                // Check for a win or a draw here if needed
+                // If the game is over, display a message or take appropriate actions
+            } else {
+                JOptionPane.showMessageDialog(GamePanel.this, "Invalid move!");
+            }
+        }
+    }
+}
