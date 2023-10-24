8c4a9fb77133810c7fc3a614c221d940cc9dfcc3
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-10-24 12:51:52 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,9 @@
-import algo.Grid;
-import javax.swing.*;
+package Algo;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Grid board = new Grid(3);
-
-            JFrame frame = new JFrame("Tic Tac Toe");
-            GamePanel gamePanel = new GamePanel(board);
-
-            frame.add(gamePanel);
-            frame.pack();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-
-            while (!board.check4Win() && !board.isBoardFull()) {
-                // Handle game logic here if needed
-                int player = board.whosTurn();
-                System.out.println("Player " + player + "'s turn");
-                board.printBoard();
-                board.makeAMove();
-            }
-
-            // Handle game over logic here (display a message, etc.)
-            if (board.check4Win()) {
-                System.out.println("Player " + board.getOpponentPlayer() + " wins!");
-            } else {
-                System.out.println("It's a draw!");
-            }
-        });
+        TicTacToeGUI gui = new TicTacToeGUI();
+        gui.createAndShowGUI();
     }
 }
 
